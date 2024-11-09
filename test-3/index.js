c5f2f73dc49f3d239d8afd812ab8f1d7c9eecb42
Karishma2422 <karishmapatil74@gmail.com>
2024-11-09 14:58:17 +0530

    test3

---
+++ test-3/index.js
@@ -0,0 +1,220 @@
+// question no. 1  addition of two numbers that add up to a target
+// var array = [2, 7, 11, 15,];
+// var target = 9;
+
+// for (var i = 0; i < array.length; i++){
+//     for (var j = i + 1; j < array.length; j++){
+//         if (array[i] + array[j] == target){
+//             console.log(array[i] + " + " + array[j], " = ", target)
+//         }
+//     }
+// }
+// var array1 = [3, 2, 4];
+// var target = 6;
+
+// for (var a = 0; a < array.length; a++){
+//     for (var b = a + 1; b < array.length; b++){
+//         if (array[a] + array[b] == target){
+//             console.log(array[a] + " + " + array[b], " = ", target)
+//         }
+//     }
+// }
+
+// var array = [3, 3];
+// var target = 6;
+
+// for (var x = 0; x < array.length; x++){
+//     for (var y = x + 1; y < array.length; y++){
+//         if (array[x] + array[y] == target){
+//             console.log(array[x] + " + " + array[y], " = ", target)
+//         }
+//     }
+// }
+
+//question no 2 determine two strings anagrms 
+// here we have t use plindrome logic
+
+
+//quetion no 3 remove all occurence of a specifi element from array
+
+
+
+// question no. 4 find sum of all digit in a number.
+// function additionofnum() {
+//     var number1 = 1;
+//     var number2 = 2;
+//     var number3 = 3;
+//     console.log(number1 + number2 + number3);
+//   }
+//   additionofnum();
+//   //2nd
+//   function additionofnum() {
+//     var number1 = 4;
+//     var number2 = 5;
+//     var number3 = 6;
+//     console.log(number1 + number2 + number3);
+//   }
+//   additionofnum();
+
+// //3rd 
+// function additionofnum() {
+//     var number1 = 7;
+//     var number2 = 8;
+//     var number3 = 9;
+//     console.log(number1 + number2 + number3);
+//   }
+//   additionofnum();
+
+
+
+
+// question no 5 perform binry search on a sorted array
+// var array = [1, 2, 3, 4, 5];
+// var target = 3;
+
+
+
+// function binarysearch() {
+// var left =0;
+// var right = array.length - 1;
+
+// while (left <= right) {
+//     console.log(left, "left", right, "right" )
+//     const midIndex = Math.floor((left + right ) / 2);
+//        console.log(midIndex,"midIndex",array[midIndex], "element");
+//      if (array[midIndex] == target) {
+//         console.log(array[midIndex], target, "array[midIndex] == target");
+//         return midIndex;
+//      }else if (array[midIndex] > target){
+//         right = midIndex - 1;
+//         console.log(array[midIndex], target, "array[midIndex] > target");
+//      } else if (array[midIndex] < target){
+//         left = midIndex + 1;
+//         console.log(array[midIndex], target, "array[midIndex] > target");
+//      }
+
+//     }
+// return "not found";
+// }
+// console.log(binarysearch(array,target));
+// //2nd 
+// var array = [10, 20, 30, 40, 50];
+// var target = 25;
+
+
+
+// function binarysearch() {
+// var left =0;
+// var right = array.length - 1;
+
+// while (left <= right) {
+//     console.log(left, "left", right, "right" )
+//     const midIndex = Math.floor((left + right ) / 2);
+//        console.log(midIndex,"midIndex",array[midIndex], "element");
+//      if (array[midIndex] == target) {
+//         console.log(array[midIndex], target, "array[midIndex] == target");
+//         return midIndex;
+//      }else if (array[midIndex] > target){
+//         right = midIndex - 1;
+//         console.log(array[midIndex], target, "array[midIndex] > target");
+//      } else if (array[midIndex] < target){
+//         left = midIndex + 1;
+//         console.log(array[midIndex], target, "array[midIndex] > target");
+//      }
+
+//     }
+// return "-1";
+// }
+// console.log(binarysearch(array,target));
+//3rd 
+// var array = [5, 15, 25, 35, 45];
+// var target = 35;
+
+
+
+// function binarysearch() {
+// var left =0;
+// var right = array.length - 1;
+
+// while (left <= right) {
+//     console.log(left, "left", right, "right" )
+//     const midIndex = Math.floor((left + right ) / 2);
+//        console.log(midIndex,"midIndex",array[midIndex], "element");
+//      if (array[midIndex] == target) {
+//         console.log(array[midIndex], target, "array[midIndex] == target");
+//         return midIndex;
+//      }else if (array[midIndex] > target){
+//         right = midIndex - 1;
+//         console.log(array[midIndex], target, "array[midIndex] > target");
+//      } else if (array[midIndex] < target){
+//         left = midIndex + 1;
+//         console.log(array[midIndex], target, "array[midIndex] > target");
+//      }
+
+//     }
+// return "not found";
+// }
+// console.log(binarysearch(array,target));
+
+//question 6 move all zeros in an array to the end while maintaing the order of non-zero elemnet
+
+//here we are using pop function
+
+
+//question 7 count the numbers of prime numbers less than a given number 
+
+
+
+// qustion 8 intersection of arrays
+
+
+
+// question 9 longest common subsequence
+
+
+
+
+
+// question 10 count the occurence of  given chracter in a string 
+
+
+
+// question 11 find minimum distnce between two element in an array 
+
+
+// qusetion 12 maximun product of two elements in an array.
+
+
+// question 13 calculate the numbers of steps to reduce a number to zero , whwre if its even divide by 2 and if its odd and subtract 1
+
+
+
+// question 14 return an array of duplicates found in the input array.
+
+
+// question 15 find peak element in an array where an elemnt is gratrer than its neighbours
+
+
+// question 16 check if an array is sorted in asecding order
+
+
+// question 17 genrate nth row of pascal triangle
+
+
+// question 18 find the sum of even fibonacci numbers up to a given  numbers
+
+
+// question 19 find the lnght of the last word in a given string
+
+
+//question 20 count the number of distinct elemnets in an array
+
+ 
+
+
+
+
+
+
+
+
